56c62c9c14984ac035fe42dc12decce299900c6f
Jakub Rychlý <jrychly@jakubrychly.cz>
2018-06-09 10:47:35 +0200

    Add place change

--- js/data-toogle.js
+++ js/data-toogle.js
@@ -3,3 +3,11 @@ $('body').bootstrapMaterialDesign();
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+var busplace = "Kladno";
+
+var place = function(value) {
+    var trip_place = value;
+    console.log(trip_place);
+    $(".trip_place").html(trip_place);
+}
