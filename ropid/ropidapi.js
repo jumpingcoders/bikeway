c7c8348fd3715b531d3489bfcfc57df093ce6fb2
Martin Hajný <hajnina11@gmail.com>
2018-06-09 09:15:16 +0200

    implementation 0.6

--- ropid/ropidapi.js
+++ ropid/ropidapi.js
@@ -1,26 +1,24 @@
-function getDelay(var tripId){
+function getDelay(tripId){
   $.getJSON("testdata.json", function (data) {
     for(var i=0; i<data.tripUpdates.length; i++){
       if(data.tripUpdates[i].tripId==tripId){
-        var vystup=Math.floor(data.tripUpdates[i].delay/60);
+        return Math.floor(data.tripUpdates[i].delay/60);
       }
     }
   });
-  return vystup;
 }
 
-function getIndex(var tripId){
+function getIndex(tripId){
   $.getJSON("testdata.json", function (data) {
   for(var i=0; i<data.tripUpdates.length; i++){
     if(data.tripUpdates[i].tripId==tripId){
-      var vystup = i;
-      break;
+      return i;
     }
   }
-  return vystup;
+});
 }
 
-function getBusNum(var tripId){
+function getBusNum(tripId){
   $.getJSON("testdata.json", function (data) {
   for(var i=0; i<data.tripUpdates.length; i++){
     if(data.tripUpdates[i].tripId==tripId){
@@ -28,48 +26,46 @@ function getBusNum(var tripId){
     }
   }
   return vystup;
+});
 }
 
-function getNextStop(var tripId){
-  $.getJSON("testdata.json", function (busdata1) {
-    var busdata = busdata1
-  }
+function getNextStop(tripId){
+  $.getJSON("testdata.json", function (busdata) {
   $.get("jizdnirad/stop_times.csv", function (data) {
     var stops = data.split("\n");
     for(var i=0; i<stops.length; i++){
         var params = stops[i].split(",");
         if(params[0]==tripId){
-          if(params[3]==busdata.tripUpdates[getIndex(tripId)].measuredStopId){
+          if(params[3]==busdata.tripUpdates[/*getIndex(tripId)*/27].measuredStopId){
             var nextStopId=stops[i+1].split(",")[3];
           }
         }
     }
-  }
-  $.get("jizdnirad/stops.csv", function (data) {
-    var stops=data.split("\n");
-    for (var i=0; i<stops.length; i++){
-      if(stops[i].split(",")[0]==nextStopId){
-        return stops[i].split(",")[1];
+    $.get("jizdnirad/stops.csv", function (data) {
+      var stops=data.split("\n");
+      for (var i=0; i<stops.length; i++){
+        if(stops[i].split(",")[0]==nextStopId){
+          return stops[i].split(",")[1];
+        }
       }
-    }
-  }
-});
-  return vystup;
+    });
+  });
+  });
+
 }
 
-function nextStopETA(var tripId){
-  $.getJSON("testdata.json", function (busdata1) {
-    var busdata = busdata1
-  }
+function nextStopETA(tripId){
+  $.getJSON("testdata.json", function (busdata){
   $.get("jizdnirad/stop_times.csv", function (data) {
     var stops = data.split("\n");
     for(var i=0; i<stops.length; i++){
         var params = stops[i].split(",");
         if(params[0]==tripId){
-          if(params[3]==busdata.tripUpdates[getIndex(tripId)].measuredStopId){
+          if(params[3]==busdata.tripUpdates[/*getIndex(tripId)*/27].measuredStopId){
             var nextStopTime=stops[i+1].split(",")[1]; //nějak přičíst zpoždění (v sekundách)
           }
         }
     }
-  }
+  });
+  });
 }
